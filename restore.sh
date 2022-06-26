af78378cfad5c3575e7cba884474266f1db1c1d9
Haunui Saint-sevin <haunui@saint-sevin.fr>
2022-06-26 08:31:33 +0200

    Variabilize SSH_OPTS

--- restore.sh
+++ restore.sh
@@ -2,9 +2,11 @@
 
 DATABASE="webapp"
 
+SSH_OPTS="-o StrictHostKeyChecking=no"
+
 f=0
 
-if ! ssh -o StrictHostKeyChecking=no $BKP_SSH_LOGIN "cat $BKP_PATH/instance_ip" < /dev/null > instance_ip; then
+if ! ssh $SSH_OPTS $BKP_SSH_LOGIN "cat $BKP_PATH/instance_ip" < /dev/null > instance_ip; then
   echo "No instance found"
   echo "Nothing to do."
   exit 0
@@ -12,7 +14,6 @@ fi
 
 IP=$(cat instance_ip)
 
-SSH_OPTS="-o StrictHostKeyChecking=no"
 SSH_LOGIN="$SSH_USER@$IP"
 
 bkp_files=
@@ -33,7 +34,7 @@ while IFS= read -r line; do
     break
   fi
 
-done < <(ssh -o StrictHostKeyChecking=no $BKP_SSH_LOGIN "ls -t $BKP_PATH | grep -v instance_ip")
+done < <(ssh $SSH_OPTS $BKP_SSH_LOGIN "ls -t $BKP_PATH | grep -v instance_ip")
 
 if [ $f -gt 0 ]; then
   f=0
@@ -41,7 +42,7 @@ if [ $f -gt 0 ]; then
     f=$(($f+1))
 
     echo "Download $line"
-    rsync -e "ssh -o StrictHostKeyChecking=no" -az $BKP_SSH_LOGIN:$BKP_PATH/$line .
+    rsync -e "ssh $SSH_OPTS" -az $BKP_SSH_LOGIN:$BKP_PATH/$line .
 
     echo "Restore $line"
     scp $SSH_OPTS $line $SSH_LOGIN:~ < /dev/null
